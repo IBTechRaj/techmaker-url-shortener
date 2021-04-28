4ddb2dd767c6ae10dbe2ac60791e80a316f5f960
IBTechRaj <raj_shk@rediffmail.com>
2021-04-28 22:13:56 +0530

    controller tested with rspec

--- app/services/shortener.rb
+++ app/services/shortener.rb
@@ -2,17 +2,33 @@ require "digest/sha2"
 
 class Shortener
 
-    attr_reader :url
+    attr_reader :url, :link_model
 
-    def initialize(url)
+    def initialize(url, link_model = Link)
         @url = url
+        @link_model=link_model
     end
 
     def generate_short_link
-Link.create(original_url: url, lookup_code: lookup_code)
+link_model.create(original_url: url, lookup_code: lookup_code)
     end
 
+
+ 
     def lookup_code
-Digest::SHA256.hexdigest(url)[0..6]
+        # 1. get a lookup code
+        # 2. check if it already exists
+        # 3. break out of the loop if it does not exists
+        # 4. if not, get another code
+    loop do
+        code = get_fresh_code
+        break code unless link_model.exists?(lookup_code: code)
+        i=i+1
+    end
+    end
+private
+    def get_fresh_code
+        # Digest::SHA256.hexdigest(url)[i..(i+6)]
+        SecureRandom.uuid[0..6]
     end
 end
